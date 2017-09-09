d4c15d7043846b42c5598ad1b4f4c315edbe9381
Andrew Eskenazi <drewesk@gmail.com>
2017-09-09 17:24:47 -0600

    Adding middleware

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const Koa = require('koa');
 const app = new Koa();
 
+const bodyParser = require('koa-bodyparser');
+
 const index = require('./routes/index');
 const movies = require('./routes/movies');
 
+app.use(bodyParser());
 app.use(index.routes());
 app.use(movies.routes());
 
